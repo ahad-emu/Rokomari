e880b04e254646aab6be6ff19bb4acef8a066638
ahad-emu <ahademu99@gmail.com>
2019-10-01 02:41:18 +0600

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,22 @@
+function input_search(){
+    document.getElementById("input_search").style.borderStyle="none";
+}
+
+function first_menu_show(){
+    document.getElementById("first_menu").style.display = "block";
+}
+function first_menu_hide(){
+    document.getElementById("first_menu").style.display = "none";
+}
+function second_menu_show(){
+    document.getElementById("second_menu").style.display = "block";
+}
+function second_menu_hide(){
+    document.getElementById("second_menu").style.display = "none";
+}
+function third_menu_show(){
+    document.getElementById("third_menu").style.display = "block";
+}
+function third_menu_hide(){
+    document.getElementById("third_menu").style.display = "none";
+}
